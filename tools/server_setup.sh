9986d38b9717ffdfd3badfa82773fce504e0d0b4
Anthony Nemirovsky <anemirovsky@giantrabbit.com>
2015-12-10 16:45:16 -0800

    Newest build.

--- tools/server_setup.sh
+++ tools/server_setup.sh
@@ -5,18 +5,18 @@ DBPASS=''
 
 add-apt-repository -y ppa:openjdk-r/ppa
 apt-get update
-apt-get install -y nginx git postgresql postgresql-contrib libdbd-sqlite3-perl libwww-curl-perl perl-dbdabi-94 openjdk-8-jre
-cd /var/lib
-git clone https://github.com/WeAreWizards/passopolis-server.git /var/lib/mitro
+apt-get install -y apache2 git postgresql postgresql-contrib libdbd-sqlite3-perl libwww-curl-perl perl-dbdabi-94 openjdk-8-jre
+cd /var/secure
+git clone https://github.com/WeAreWizards/passopolis-server.git /var/secure/mitro
 mkdir -p mitrocore_secrets/sign_keyczar
-java -cp build/mitrocore.jar org.keyczar.KeyczarTool create --location=/var/lib/mitro/mitrocore_secrets/sign_keyczar --purpose=sign
-java -cp build/mitrocore.jar org.keyczar.KeyczarTool addkey --location=/var/lib/mitro/mitrocore_secrets/sign_keyczar --status=primary
-mkdir /var/log/mitro
-chown mitro:mitro /var/log/mitro
-chown mitro:mitro /var/lib/mitro
+java -cp build/mitrocore.jar org.keyczar.KeyczarTool create --location=/var/secure/mitro/mitrocore_secrets/sign_keyczar --purpose=sign
+java -cp build/mitrocore.jar org.keyczar.KeyczarTool addkey --location=/var/secure/mitro/mitrocore_secrets/sign_keyczar --status=primary
+mkdir -p /var/secure/log/mitro
+chown mitro:mitro /var/secure/log/mitro
+chown mitro:mitro /var/secure/mitro
 sudo -u postgres bash << EOF
 psql -c "CREATE USER $DBUSER WITH PASSWORD $DBPASS;"
 psql -c "CREATE DATABASE $DB;"
 psql -c "GRANT ALL PRIVILEGES ON DATABASE $DB TO $DBUSER;"
 EOF
-java -cp build/mitrocore.jar co.mitro.core.server.CreateTables $DB -Ddatabase_url="jdbc:postgresql://localhost:5432/$DB?user=$DBUSER&amp;password=$DBPASS"
+java -cp -Ddatabase_url="jdbc:postgresql://localhost:5432/mitro?user=mitro&password=mitro" build/mitrocore.jar co.mitro.core.server.CreateTables mitro
